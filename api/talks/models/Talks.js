f12752b5f66e9119ebf99cb66e788df82968ace6
jankaderabek <jan.kaderabek@peckadesign.cz>
2019-10-11 21:06:57 +0800

    Disable Talks static builds

--- api/talks/models/Talks.js
+++ api/talks/models/Talks.js
@@ -5,13 +5,4 @@
  */
 
 module.exports = {
-  afterCreate: async entry => {
-    strapi.services.frontend.buildStaticWebsite();
-  },
-  afterUpdate: async entry => {
-    strapi.services.frontend.buildStaticWebsite();
-  },
-  afterDestroy: async entry => {
-    strapi.services.frontend.buildStaticWebsite();
-  },
 };
